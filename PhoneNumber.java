841eafef4d84d4763e6ab40cddb65935e16b9bb0
prodhanm <mustafizur.prodhan@gmail.com>
2025-08-04 17:03:09 -0400

    modified .gitignore and added PhoneNumber.java

---
+++ PhoneNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        String[] phoneNums = new String[100];
+        String phoneNum;
+        int filledElements = 0;
+        
+        System.out.print("Enter a phone number (or q to quit): ");
+        phoneNum = inp.nextLine();
+        
+        while(!phoneNum.equalsIgnoreCase("q") 
+            && filledElements < phoneNums.length) {
+                if(phoneNum.length() < 1 || phoneNum.length() > 16) {
+                    System.out.println("This is not a valid number. Please try again!");
+                }
+                else {
+                    phoneNums[filledElements] = phoneNum;
+                    filledElements++;
+                }
+                System.out.print("Enter a phone number (or q to quit): ");
+                phoneNum = inp.nextLine();
+            }
+            
+            System.out.println("\nContact List: ");
+            for(int i=0; i < filledElements; i++) {
+                System.out.println((i+1) + ". " + phoneNums[i]);
+            }
+            
+        System.out.println("Success!");
+    }
+}
